70f98e5dc8440595a5a5aceca364124a122cca09
shanmukhavemulapalli <shanmukhavemulapalli@gmail.com>
2023-04-21 14:31:37 +0530

    1st

---
+++ fooddel/app1/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate , logout , login as loginUser
+from django.contrib import messages
+from django.urls import reverse
+
+def HomePage(request):
+    return render(request, 'home.html')
+def Signup(request):
+    
+    if request.method == 'POST':
+        uname=request.POST.get('Name')
+        email=request.POST.get("mail")
+        dob=request.POST.get("dob")
+        pass1=request.POST.get('password')
+        pass2=request.POST.get('confirm_p')
+       
+        if pass1!=pass2:
+            return HttpResponse("p1 not equla to p2")
+        else:
+           my_user=User.objects.create_user(uname,email,pass1) 
+           my_user.save()
+           return redirect('LoginPage')
+        
+    
+    return render(request, 'login.html')
+
+def login(request):
+
+       
+    if request.method == 'POST':
+         username = request.POST.get('uname') 
+         password = request.POST.get('pass') 
+
+         user = authenticate(request , username = username, password = password) 
+
+         if user is not None:
+            loginUser(request,user) 
+            return redirect('HomePage') 
+         else:
+            messages.info(request,'Username or password is incorrect') 
+            context = {} 
+        
+       
+      
+    return render(request, 'login.html')
+   
+def logoutPage(request):
+    logout(request)
+    url = reverse("login")
+    return HttpResponse(url)
+def ksburger(request):
+    return render(request,'ksbakers.html')
+
+
